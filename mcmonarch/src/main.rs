1dc23333b9e88718e65e6f05bdae35fc5edde313
Alexander Launi <alex.launi@gmail.com>
2020-09-19 17:40:51 -0400

    update host bind

--- mcmonarch/src/main.rs
+++ mcmonarch/src/main.rs
@@ -17,10 +17,12 @@ pub async fn main() -> Result<()> {
     pretty_env_logger::init();
     color_eyre::install()?;
 
+    
     let bot_token = env::var(BOT_TOKEN_ENVVAR)?;
     let web_ip = env::var(WEB_IP_ENVVAR)?;
     let web_port = env::var(WEB_PORT_ENVVAR)?;
-    let web_addr = format!("{}:{}", web_port, web_ip)
+
+    let web_addr = format!("{}:{}", web_ip, web_port)
         .parse::<SocketAddrV4>()?;
 
     let verify_box = Box::new(|data| mcmonarch_bot::McmonarchBot::verify(data).boxed()); 
